6d42c10bd43b7abd73ec2d9bdaa640be8e1d68dd
Celso Mireles <celso.mireles@gmail.com>
2017-02-22 22:20:02 -0700

    Styles the header a bit more

--- src/containers/app/Header.jsx
+++ src/containers/app/Header.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { Menu, Image } from 'semantic-ui-react';
 import logo from './logo.svg';
+import './Header.css';
 
 class Header extends Component {
   render()  {
     return (
       <nav>
-        <img src={logo} alt="logo" width="50" height="50" />
+        <Menu className="app-menu" pointing secondary>
+          <Link className="nav-logo" to={'/'}>
+            <h1>RedadAlertas</h1>
+          </Link>
+          <Menu.Menu position='right'>
+            <Image src='http://lorempixel.com/100/100/people' className="profile-icon" alt="profile" avatar />
+          </Menu.Menu>
+          </Menu>
       </nav>
     )
   }
