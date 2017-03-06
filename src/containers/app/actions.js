ba275e166d8f701a9be0409b90ce6b4f1d4f2806
bianca c [arghgr] <tenmillionpeople@gmail.com>
2017-03-05 20:13:19 -0500

    redux-saga: raid functions abstracted into modules

--- src/containers/app/actions.js
+++ src/containers/app/actions.js
@@ -1,21 +1,22 @@
 export const TOGGLE_DRAWER_ACTIVE = 'TOGGLE_DRAWER_ACTIVE';
-export function toggleDrawerActive() {
-	return {
-		type: TOGGLE_DRAWER_ACTIVE
-	};
-};
-
 export const SHOW_SNACKBAR = 'SHOW_SNACKBAR';
-export const showSnackbar = (message) => {
-	return {
-		type: SHOW_SNACKBAR,
-		message
-	};
+export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
+
+export function toggleDrawerActive() {
+  return {
+    type: TOGGLE_DRAWER_ACTIVE
+  };
 };
 
-export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
+export const showSnackbar = (message) => {
+  return {
+    type: SHOW_SNACKBAR,
+    payload: message
+  };
+};
+
 export const closeSnackbar = () => {
-	return {
-		type: CLOSE_SNACKBAR
-	};
+  return {
+    type: CLOSE_SNACKBAR
+  };
 };
--- src/containers/app/reducer.js
+++ src/containers/app/reducer.js
@@ -5,39 +5,37 @@ import {
 } from './actions';
 
 export const initialState = {
-	drawerActive: false,
-	sidebarPinned: false,
-	scrollable: false,
-	dialogActive: false,
-	snackbarActive: false,
+  drawerActive: false,
+  sidebarPinned: false,
+  scrollable: false,
+  dialogActive: false,
+  snackbarActive: false,
   snackbarMessage: '',
   snackbarSuccess: false,
-	dialogMessage: '',
-	dialogTitle: ''
+  dialogMessage: '',
+  dialogTitle: ''
 };
 
 export default function reducer(state = initialState, action) {
-	switch (action.type) {
-		case TOGGLE_DRAWER_ACTIVE:
-			return {
-				...state,
-				drawerActive: !state.drawerActive
-			};
+  switch (action.type) {
+    case TOGGLE_DRAWER_ACTIVE:
+      return {
+        ...state,
+        drawerActive: !state.drawerActive
+      };
     case SHOW_SNACKBAR:
       return {
         ...state,
-        snackbarMessage: action.message,
+        snackbarMessage: action.payload,
         snackbarActive: true
       };
-
     case CLOSE_SNACKBAR:
       return {
         ...state,
         snackbarActive: false,
         snackbarMessage: ''
       };
-
-		default:
-			return state;
-	}
+  default:
+    return state;
+  }
 }
