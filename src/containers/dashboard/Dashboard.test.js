42d2b17ae489a2668d0efdd6a35030f80ed4bd8e
Len Smith <len@barrison.com>
2017-03-07 17:02:24 -0800

    Fix Dashboard Spec
     * add redux-mock-store so tests can render components that touch storej
     * use mount to assert against DOM

--- src/containers/dashboard/Dashboard.test.js
+++ src/containers/dashboard/Dashboard.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './Dashboard';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore();
+const store = mockStore({raids: []});
 
 describe('<Dashboard />', () => {
   it('should have an element with class "dashboard"', () => {
-    const wrapper = shallow(<Dashboard />);
+    const wrapper = mount(<Dashboard store={ store } />);
     expect(wrapper.find('.dashboard')).to.have.length(1);
   });
 });
