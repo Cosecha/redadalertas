a947a899febfb531c8480fbd89d34425c3618fba
bianca c [arghgr] <tenmillionpeople@gmail.com>
2017-03-08 02:08:43 -0500

    RaidInfo: time --> date

--- src/components/RaidInfo.jsx
+++ src/components/RaidInfo.jsx
@@ -12,7 +12,7 @@ const RaidInfo = (props) => {
 
   return (
     <div className="raidInfo">
-      <div className="raidStat col-xs-2"> <h1>{t('time')}</h1> <p>{date}</p> </div >
+      <div className="raidStat col-xs-2"> <h1>{t('date')}</h1> <p>{date}</p> </div >
       <div className="raidStat col-xs-2"> <h1>{t('location')}</h1> <p>{location}</p> </div >
       <div className="raidStat col-xs-2"> <h1>{t('type')}</h1> <p>{type}</p> </div >
       <div className="raidStat col-xs-2"> <h1>{t('description')}</h1> <p>{description}</p> </div >
