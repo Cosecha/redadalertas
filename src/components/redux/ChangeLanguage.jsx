d3c316a5679a10df77a9106f81a70cab76058c93
Len Smith <len@barrison.com>
2017-03-14 01:09:54 -0700

    Refactor ChangeLanguage Component

--- src/components/redux/ChangeLanguage.jsx
+++ src/components/redux/ChangeLanguage.jsx
@@ -3,35 +3,19 @@ import { connect }          from 'react-redux';
 import { setLanguage }      from 'redux-i18n';
 
 class ChangeLanguage extends Component {
-  constructor(props) {
-    super(props);
-    this.setSpanish = this.setSpanish.bind(this);
-    this.setEnglish = this.setEnglish.bind(this);
-    this.setFrench = this.setFrench.bind(this);
-  }
-
-  setSpanish() { this.props.dispatch(setLanguage("es")); }
-  setEnglish() { this.props.dispatch(setLanguage("en")); }
-  setFrench() { this.props.dispatch(setLanguage("fr")); }
-
   render() {
-    const { currentLang } = this.props;
-
-    const spanishButton = (currentLang === 'es') ? '' : (
-      <button onClick={this.setSpanish.bind(this)}>Español</button>
-    );
-    const englishButton = (currentLang === 'en') ? '' : (
-      <button onClick={this.setEnglish.bind(this)}>English</button>
-    );
-    const frenchButton = (currentLang === 'fr') ? '' : (
-      <button onClick={this.setFrench.bind(this)}>Français</button>
-    );
+    const { dispatch, currentLang } = this.props;
 
     return (
       <div>
-        {spanishButton}
-        {englishButton}
-        {frenchButton}
+        {currentLang !== 'es' &&
+           <button onClick={() => dispatch(setLanguage("es"))}>Español</button>}
+
+        {currentLang !== 'en' &&
+           <button onClick={() => dispatch(setLanguage("en"))}>English</button>}
+
+        {currentLang !== 'fr' &&
+           <button onClick={() => dispatch(setLanguage("fr"))}>Français</button>}
       </div>
     )
   }
