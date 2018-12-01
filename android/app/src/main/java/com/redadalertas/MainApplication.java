0913abb81f039c569f4be829d4d8b087a66a9d86
Alex Graffeo-Cohen <alex@graffeocohen.com>
2018-12-01 14:52:09 -0500

    Add ReactNative Maps package

--- android/app/src/main/java/com/redadalertas/MainApplication.java
+++ android/app/src/main/java/com/redadalertas/MainApplication.java
@@ -3,6 +3,7 @@ package com.redadalertas;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage()
       );
