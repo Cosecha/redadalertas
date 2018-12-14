5f1b132eaff40fcf28b9537ba5017297a2521656
Alex Graffeo-Cohen <alex@graffeocohen.com>
2018-12-14 09:28:59 -0500

    WIP: start reporter form

--- src/components/ReportForm.js
+++ src/components/ReportForm.js
@@ -1,31 +1,69 @@
 // Setup
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { Picker, StyleSheet, View } from "react-native";
 
 // Vendor
-import { Button, Text } from "react-native-elements";
+import {
+  Button,
+  CheckBox,
+  FormLabel,
+  FormInput,
+  FormValidationMessage
+} from "react-native-elements";
+import { Formik } from "formik";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "#F5FCFF"
   }
 });
 
+const types = [{ label: "Sweep", value: "sweep" }];
+
 export default class ReportForm extends Component {
   render() {
     const { navigation } = this.props;
+    const initialValues = {
+      expire: {
+        deleteOnExpire: false
+      }
+    };
 
     return (
-      <View style={styles.container}>
-        <Button
-          borderRadius={5}
-          onPress={() => navigation.navigate("ReporterLoginForm")}
-          title="Sign Out"
-        />
-      </View>
+      <Formik initialValues={initialValues} onSubmit={this.onSubmit}>
+        {props => (
+          <View style={styles.container}>
+            <FormLabel>Description</FormLabel>
+            <FormInput
+              multiline
+              onChangeText={props.handleChange("description")}
+            />
+
+            <Picker
+              onValueChange={props.handleChange("type")}
+              style={{ height: 50, width: 100 }}
+              value={props.values.type}
+            >
+              {types.map(type => (
+                <Picker.Item
+                  key={type.value}
+                  label={type.label}
+                  value={type.value}
+                />
+              ))}
+            </Picker>
+
+            <CheckBox
+              checked={props.values.expire.deleteOnExpire}
+              onPress={props.handleChange("expire.deleteOnExpire")}
+              title="Delete on Expire"
+            />
+          </View>
+        )}
+      </Formik>
     );
   }
 }
