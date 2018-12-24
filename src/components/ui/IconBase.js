1650daeeef0c109380a235f6f8541ab94363fab3
Alex Graffeo-Cohen <alex@graffeocohen.com>
2018-12-24 18:46:30 -0500

    Push Platform knowledge into <IconBase>

--- src/components/ui/IconBase.js
+++ src/components/ui/IconBase.js
@@ -1,13 +1,20 @@
 // Setup
 import React, { Component } from "react";
+import { Platform } from "react-native";
 import PropTypes from "prop-types";
 
 // Vendor
 import { Icon } from "native-base";
 
-const IconBase = ({ color, name, size }) => (
-  <Icon name={name} type="Ionicons" style={{ color: color, fontSize: size }} />
-);
+const IconBase = ({ color, name, size }) => {
+  const iconName = Platform.select({
+    ios: `ios-${name}`,
+    android: `md-${name}`
+  });
+  return (
+    <Icon name={iconName} type="Ionicons" style={{ color, fontSize: size }} />
+  );
+};
 
 IconBase.propTypes = {
   color: PropTypes.string,
--- src/components/ui/TabBarIcon.js
+++ src/components/ui/TabBarIcon.js
@@ -1,18 +1,13 @@
 // Setup
 import React, { Component } from "react";
-import { Platform } from "react-native";
 import PropTypes from "prop-types";
 
 // Redadalertas
 import IconBase from "ui/IconBase";
 
-const TabBarIcon = ({ color, name }) => {
-  const iconName = Platform.select({
-    ios: `ios-${name}`,
-    android: `md-${name}`
-  });
-  return <IconBase color={color} name={iconName} size={25} />;
-};
+const TabBarIcon = ({ color, name }) => (
+  <IconBase color={color} name={name} size={25} />
+);
 
 TabBarIcon.propTypes = {
   color: PropTypes.string.isRequired,
