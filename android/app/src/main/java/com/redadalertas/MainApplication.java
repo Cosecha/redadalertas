27c7b132b0f5a91b587f5ffe338e01e984f1da81
Alex Graffeo-Cohen <alex@graffeocohen.com>
2018-12-24 11:10:58 -0500

    Add react-native-geocoder

--- android/app/src/main/java/com/redadalertas/MainApplication.java
+++ android/app/src/main/java/com/redadalertas/MainApplication.java
@@ -3,6 +3,7 @@ package com.redadalertas;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.devfd.RNGeocoder.RNGeocoderPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGeocoderPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage()
