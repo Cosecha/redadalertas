081888110f86105884a3b53ce66318a353b17e24
bianca c [arghgr] <tenmillionpeople@gmail.com>
2019-01-17 20:47:57 -0500

    Add babel package to use api as actual env var
    babel-plugin-transform-inline-environment-variables

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import env from "../../env.json";
 
 const orgApi = axios.create({
-  baseURL: env.SERVICE_API_URL
+  baseURL: process.env.SERVICE_API_URL
 });
 
 export default orgApi;
