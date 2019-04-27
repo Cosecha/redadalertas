fd7bfc44f4c04935c2460dbe9a13a59448d0a388
Alex Graffeo-Cohen <alex@graffeocohen.com>
2019-04-27 12:52:19 -0400

    Configure notifications for iOS

--- ios/Redadalertas/AppDelegate.m
+++ ios/Redadalertas/AppDelegate.m
@@ -14,6 +14,8 @@
 #import <React/RCTRootView.h>
 
 #import <Firebase.h>
+#import "RNFirebaseMessaging.h"
+#import "RNFirebaseNotifications.h"
 
 @implementation AppDelegate
 
@@ -28,6 +30,7 @@
   [AppCenterReactNative register];  // Initialize AppCenter
 
   [FIRApp configure]; // Initialize Firebase
+  [RNFirebaseNotifications configure]; // Initialize Firebase Notifications
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
@@ -43,6 +46,29 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+#pragma mark - push notifications
+// Required for the notification event. You must call the completion handler after handling the remote notification.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo
+                                                       fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
+  [[RNFirebaseNotifications instance] didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+
+// Required to register for notifications
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+  [[RNFirebaseMessaging instance] didRegisterUserNotificationSettings:notificationSettings];
+}
+
+-(void) userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+
+  [[RNFirebaseMessaging instance] didReceiveRemoteNotification:response.notification.request.content.userInfo];
+  completionHandler();
+}
+
+// Required for the localNotification event.
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+    [[RNFirebaseNotifications instance] didReceiveLocalNotification:notification];
 }
 
 @end
