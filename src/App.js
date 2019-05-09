04123fb73bc8c075abc4379a26dfbe11327889a8
Alex Graffeo-Cohen <alex@graffeocohen.com>
2019-05-08 21:39:55 -0400

    Add and configure redux-persist
    This allows us to store our redux state in AsyncStorage so that if the
    app loses memory, we will still retain the app state. This is
    particularly important for events (and later alerts) because
    
    1. It is important for users to retain downloaded data as long as
    possible.
    2. We will be much less likely to falsely notify users of new events
    (and later alerts) that they had already seen.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // Vendor
 import React, { Component } from "react";
+import { ActivityIndicator } from "react-native";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import firebase from "react-native-firebase";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 
 // Redadalertas
@@ -13,7 +15,7 @@ import Settings from "navigation/tabs/Settings";
 import { colors } from "styles";
 import { TabIcon } from "navigation/utils";
 import TabBarIcon from "ui/TabBarIcon";
-import { store } from "redux/configureStore";
+import { persistor, store } from "redux/configureStore";
 
 const persistenceKey = __DEV__ ? "NavigationState" : null;
 
@@ -63,9 +65,16 @@ export default class App extends Component {
 
     return (
       <Provider store={store}>
-        <Root>
-          <AppContainer persistenceKey={null} />
-        </Root>
+        <PersistGate
+          loading={
+            <ActivityIndicator color={colors.primary} size="large" />
+          }
+          persistor={persistor}
+        >
+          <Root>
+            <AppContainer persistenceKey={null} />
+          </Root>
+        </PersistGate>
       </Provider>
     );
   }
