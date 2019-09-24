f5b3f872cd561505a632479eb5639a91bdf0c599
bianca c [arghgr] <tenmillionpeople@gmail.com>
2019-09-24 10:24:37 -0700

    Errors: add device to redux, use implicit returns

--- src/reducers/error.js
+++ src/reducers/error.js
@@ -1,13 +1,17 @@
 export const SET_EVENTS_ERROR = "SET_EVENTS_ERROR";
 export const SET_USER_ERROR = "SET_USER_ERROR";
+export const SET_DEVICE_ERROR = "SET_DEVICE_ERROR";
 
 const initialState = {
+  device: null,
   event: null,
   user: null
 };
 
 export default function errorsReducer(state = initialState, action) {
   switch (action.type) {
+    case SET_DEVICE_ERROR:
+      return { ...state, device: action.payload.error };
     case SET_EVENTS_ERROR:
       return { ...state, event: action.payload.error };
     case SET_USER_ERROR:
@@ -17,16 +21,17 @@ export default function errorsReducer(state = initialState, action) {
   }
 }
 
-const setEventsError = error => {
-  return {
-    type: SET_EVENTS_ERROR,
-    payload: { error }
-  }
-};
+const setEventsError = error => ({
+  type: SET_EVENTS_ERROR,
+  payload: { error }
+});
 
-const setUserError = error => {
-  return {
-    type: SET_USER_ERROR,
-    payload: { error }
-  }
-};
+const setUserError = error => ({
+  type: SET_USER_ERROR,
+  payload: { error }
+});
+
+const setDeviceError = error => ({
+  type: SET_DEVICE_ERROR,
+  payload: { error }
+});
