d505ac54777ff135651b16407195c896fe92bab7
Joel Cretan <jcretan@gmail.com>
2020-03-07 20:35:26 -0500

    initial link with Tor. loads Tor test page in index.js!
    HOWEVER, there is something not committed here that is necessary.
    We need to modify node_modules/react-native/React/Base/RCTBridgeModule.h to backport some changes from a newer react-native, per this example: https://github.com/jdanbrown/birdgram/commit/da548212bb1b43644095336ab5fd4447e596b348
    May include a patch file to make this change? Upgrading react-native to v59.3 or v60 could solve this.

--- index.js
+++ index.js
@@ -4,4 +4,32 @@ import { AppRegistry } from "react-native";
 import App from "./src/App";
 import { name as appName } from "./app.json";
 
+import {NativeModules} from 'react-native';
+
 AppRegistry.registerComponent(appName, () => App);
+
+
+function downloadCallback(success, dataString) {
+	if (success) {
+		console.log(dataString)
+	} else {
+		console.log("not success =(")
+		console.log("this is probably an error message: " + dataString)
+	}
+}
+
+var TorURLDownloaderX = NativeModules.TorURLDownloaderJSBridge
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+console.log(NativeModules)
+console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+TorURLDownloaderX.download('https://check.torproject.org', downloadCallback);
